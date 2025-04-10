f3a408dd01542e09c065b27563f8e590826ce447
Kevin Kandolf <kan231017@spengergasse.at>
2025-04-10 20:11:06 +0200

    BMI

---
+++ 2025-04-10/plf.js
@@ -0,0 +1,44 @@
+class Person {
+    constructor(name, gender, groesseM, gewichtKG) {
+        if (gender !== 'm' && gender !== 'f') {
+            throw new Error('wrong gender');
+        }
+        this.name = name;
+        this.gender = gender;
+        this.groesseM = groesseM;
+        this.gewichtKG = gewichtKG;
+    }
+    groesseCM() {
+        return (this.groesseM * 100);
+    }
+    vorName() {
+        return this.name.split(' ')[0];
+    }
+    nachName() {
+        return this.name.split(" ")[1];
+    }
+
+
+
+    toString() {
+        return `${this.vorName()} ${this.nachName()} (${this.groesseCM()}cm, ${this.gewichtKG}kg)`;
+    }
+    getBmi() {
+        return this.gewichtKG / (this.groesseM * this.groesseM)
+    }
+    getGewichtType() {
+        if (this.gender === 'f') {
+            if (this.getBmi() < 19) return "Untergewicht";
+            if (this.getBmi() > 24) return "Übergewicht";
+            return "Normalgewicht";
+        }
+        if (this.gender === 'm') {
+            if (this.getBmi() < 20) return "Untergewicht";
+            if (this.getBmi() > 25) return "Übergewicht";
+            return "Normalgewicht";
+        }
+
+    }
+}
+
+export { Person };
